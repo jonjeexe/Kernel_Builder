97f06bd5bf9d546661498d563883ae58432b5d40
Jonje <168655485+jonjeexe@users.noreply.github.com>
2025-10-13 17:38:44 +0530

    Update and rename Compiler.sh to Builder.sh

--- Builder.sh
+++ Builder.sh
@@ -1,18 +1,21 @@
 #!/system/bin/sh
 clear
+echo ""
 echo "- Date : $(date)"
 echo "   ____ ___  __  __ ____  _   _     _____ ____  "
 echo "  / ___/ _ \|  \/  |  _ \| | | |   | ____|  _ \ "
 echo " | |  | | | | |\/| | |_) | | | |   |  _| | |_) |"
 echo " | |__| |_| | |  | |  __/| | | |___| |___|  _ < "
 echo "  \____\___/|_|  |_|_|   |_| |_____|_____|_| \_\ "
-echo "                Kernel compiler by @Jonjeexe                   "
+echo "               Kernel Builder by @Jonjeexe                  "
 echo ""
 echo "##################################"
 echo "## CLEANING BUILDING KERNEL SOURCE" 
 echo "##################################"
 echo ""
+echo "###########################"
 echo "# SET ENVIRONMENT VARIABLES"
+echo "###########################"
 export KBUILD_BUILD_USER="Jonje"
 export KBUILD_BUILD_HOST="Ubuntu"
 export ARCH=arm64
@@ -21,14 +24,18 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${CLANG_TRIPLE}
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 echo ""
+echo "###########################"
 echo "# CREATING OUTPUT DIRECTORY"
+echo "###########################"
 sudo mkdir -p out
 sleep 1
 echo ""
+echo "###################################"
 echo "# GENETATOR .CONFIG FILE MY KERNEL"
+echo "###################################"
 sudo make O=out ARCH=$ARCH CC="$CC" blossom_defconfig
 echo ""
-echo "# START KERMEL COMPILING "
+echo "#### START KERNEL COMPILING ####"
 sudo make -j$(nproc) O=out ARCH=$ARCH CC="$CC" \
   CROSS_COMPILE=${CROSS_COMPILE} \
   CROSS_COMPILE_ARM32=${CROSS_COMPILE_ARM32} \
